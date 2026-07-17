dbc26d02261d12e8365ed544db1b8b7874d5ad28
akashdatkar <apdatkar2004@gmail.com>
2026-07-17 20:24:46 +0530

    Ready Backend for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,8 @@ const connectDB=require('./src/config/database');
 
 
 connectDB();
+const PORT = process.env.PORT;
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('Server is running on port 3000');
 });
